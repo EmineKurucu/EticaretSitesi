16f1cadc1ce1975f4fe1a6d879d437a9c315fdb6
Emine <emine.kurucu.7@gmail.com>
2025-07-11 17:49:58 +0300

    cors hatası düzeltildi

--- Renart_server/server.js
+++ Renart_server/server.js
@@ -3,8 +3,13 @@ const cors = require("cors");
 const app = express();
 const productRoutes = require("./routes/routes");
 
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://renart-six.vercel.app"
+  ]
+}));
 
-app.use(cors()); 
 app.use(express.json());
 app.use("/products", productRoutes);
 
